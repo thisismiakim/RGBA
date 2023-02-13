d6352e7b68eaa670d41801b87d7100b77aa1be79
thisismiakim <helloiammkim@gmail.com>
2023-02-13 05:00:11 +0000

    initial commit of project

---
+++ include/RGBA.h
@@ -0,0 +1,33 @@
+#ifndef RGBA_H_
+#define RGBA_H_
+
+
+#endif
+#include <cstdint>
+
+struct RGBA
+{
+    RGBA()=default;
+    RGBA(unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a)
+    {
+        r=_r;
+        g=_g;
+        b=_b;
+        a=_a;
+    }
+
+
+    union
+    {
+    uint32_t pixels=0x000000FF;
+        struct 
+        {
+            unsigned char r;
+            unsigned char g;
+            unsigned char b;
+            unsigned char a;
+        };
+    };
+    
+
+};
--- tests/RGBATests.cpp
+++ tests/RGBATests.cpp
@@ -1,8 +1,26 @@
 #include <gtest/gtest.h>
+#include "RGBA.h"
 
-int main(int argc, char **argv)
+// int main(int argc, char **argv)
+// {
+//   testing::InitGoogleTest(&argc,argv);
+//   return RUN_ALL_TESTS();
+// }
+
+TEST (RGBA, construct)
 {
-  testing::InitGoogleTest(&argc,argv);
-  return RUN_ALL_TESTS();
+    RGBA a;
+    ASSERT_TRUE(a.r == 0);
+    ASSERT_TRUE(a.g == 0);
+    ASSERT_TRUE(a.b == 0);
+    ASSERT_TRUE(a.a == 0);
 }
 
+TEST(RGBA,construct_from_values)
+{
+    auto a=RGBA(255,128,32,12);
+    ASSERT_TRUE(a.r == 255);
+    ASSERT_TRUE(a.g == 128);
+    ASSERT_TRUE(a.b == 32);
+    ASSERT_TRUE(a.a == 12);
+}
